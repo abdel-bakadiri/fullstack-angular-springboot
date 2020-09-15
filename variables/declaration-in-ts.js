f25f18bf946c51073106c11055b75d8f5225f764
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-15 17:26:46 +0200

    Feat(): declarations in TS

---
+++ variables/declaration-in-ts.js
@@ -0,0 +1,9 @@
+var found = true;
+var score = 100;
+var firstName = "Abdel";
+var contry = "France";
+var city = "Paris";
+console.log(found);
+console.log(score);
+console.log(firstName + " live in" + city);
+console.log(firstName + " has a score " + score);
---
+++ variables/declaration-in-ts.ts
@@ -0,0 +1,9 @@
+let found = true;
+let score = 100;
+let firstName = "Abdel";
+let contry = "France";
+let city = "Paris";
+console.log(found);
+console.log(score);
+console.log(firstName + " live in" + city);
+console.log(`${firstName} has a score ${score}`);
