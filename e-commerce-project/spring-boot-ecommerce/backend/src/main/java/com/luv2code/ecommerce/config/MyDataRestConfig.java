70cede2221b55245ff62aa3e2302fbb4be88a606
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-09-25 14:48:09 +0200

    Feat(): Bakend e-commerce project

---
+++ e-commerce-project/spring-boot-ecommerce/backend/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,30 @@
+package com.luv2code.ecommerce.config;
+
+import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedAction = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable http methods for product: PUT, POST and Delete
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedAction));
+
+
+    }
+
+}
